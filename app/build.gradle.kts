cc0197102c21c8a7d12597d7073659152c34ed85
yohannesTz <yohannes222ethiopia@gmail.com>
2025-03-08 13:49:48 +0300

    chore: version bump in build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.github.yohannestz.satori"
         minSdk = 24
         targetSdk = 35
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
